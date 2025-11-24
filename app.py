94552291fc1f710dc04fe883e361c43cf4b86b9a
Gintoki-G <bambi110723@naver.com>
2025-11-25 02:04:59 +0900

    회원 가입 페이지

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 app.secret_key = "test"
 
 USERS = {
-    "admin@naver.com": {"password": "123400", "balance": 500000}
+    "admin@naver.com": {"password": "123400", "balance": 500000, "name" : "김예나"}
 }
 
 #flask의 기본구조
@@ -13,11 +13,11 @@ USERS = {
 
 @app.route('/',methods=['GET', 'POST'])
 def login() :
-    if request.method == "Post" :
+    if request.method == "POST" :
         useremail = request.form['email']
         password = request.form['password']
 
-        user = USERS.get('useremail')
+        user = USERS.get(useremail)
         #user = {"password": "1234", "balance": 500000}
         #if True and user["password"] == password:
 
@@ -28,5 +28,29 @@ def login() :
             return "로그인 실패"
     return render_template("login.html")
 
+@app.route('/newAccount',methods=['GET','POST'])
+def newAccount():
+    if request.method == "POST":
+        useremail = request.form['email']
+        password = request.form['password']
+        password2 = request.form['pw2']
+        name = request.form['name']
+        if password == password2 : 
+            if useremail in USERS :
+                return "이미 회원가입이 되어있습니다"
+            else : 
+                USERS[useremail] = {
+                "password": password,
+                "balance" : 50000,
+                "name" : name
+                }
+                return redirect(url_for("login"))
+        else : 
+            return "비밀번호가 다릅니다."
+    return render_template("newAccount.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return "대시보드 준비중!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
